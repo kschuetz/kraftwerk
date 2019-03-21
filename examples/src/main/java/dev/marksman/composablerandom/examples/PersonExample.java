d54c5a7c34aaa2d0440b43319984654f9c0602c6
Kevin Schuetz <schuetzk@gmail.com>
2019-03-21 18:03:21 -0500

    Move to multi-module project

---
+++ examples/src/main/java/dev/marksman/composablerandom/examples/PersonExample.java
@@ -0,0 +1,11 @@
+package dev.marksman.composablerandom.examples;
+
+import dev.marksman.composablerandom.builtin.Generators;
+
+public class PersonExample {
+    public static void main(String[] args) {
+        //TODO
+        Generators.generateInt();
+        System.out.println("TODO: PersonExample");
+    }
+}
