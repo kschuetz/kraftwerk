5afb73fb1c5861dbc473128bca138ec65e9e9d6c
Kevin Schuetz <schuetzk@gmail.com>
2019-03-26 17:59:09 -0500

    TODO comment on Context

--- composable-random/src/main/java/dev/marksman/composablerandom/Context.java
+++ composable-random/src/main/java/dev/marksman/composablerandom/Context.java
@@ -1,5 +1,8 @@
 package dev.marksman.composablerandom;
 
+// TODO: stacks and restore methods not necessary
+// updater of context should be responsible for remembering old value
+
 public interface Context {
     SizeParameters getSizeParameters();
 
