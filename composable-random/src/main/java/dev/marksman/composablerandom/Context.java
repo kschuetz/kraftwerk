4d499a1ac38ca99befe51d65c3984a531d5c0f17
Kevin Schuetz <schuetzk@gmail.com>
2019-03-26 17:52:20 -0500

    Add application data stack to Context

--- composable-random/src/main/java/dev/marksman/composablerandom/Context.java
+++ composable-random/src/main/java/dev/marksman/composablerandom/Context.java
@@ -3,7 +3,13 @@ package dev.marksman.composablerandom;
 public interface Context {
     SizeParameters getSizeParameters();
 
+    Object getApplicationData();
+
     Context withSizeParameters(SizeParameters sizeParameters);
 
+    Context withApplicationData(Object applicationData);
+
     Context restoreSizeParameters();
+
+    Context restoreApplicationData();
 }
--- composable-random/src/main/java/dev/marksman/composablerandom/StandardContext.java
+++ composable-random/src/main/java/dev/marksman/composablerandom/StandardContext.java
@@ -1,5 +1,6 @@
 package dev.marksman.composablerandom;
 
+import com.jnape.palatable.lambda.adt.Unit;
 import lombok.AllArgsConstructor;
 
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Head.head;
@@ -9,9 +10,10 @@ import static java.util.Collections.emptyList;
 
 @AllArgsConstructor
 public class StandardContext implements Context {
-    private static final StandardContext DEFAULT_CONTEXT = context(emptyList());
+    private static final StandardContext DEFAULT_CONTEXT = context(emptyList(), emptyList());
 
     private final Iterable<SizeParameters> sizeParametersStack;
+    private final Iterable<Object> applicationDataStack;
 
     @Override
     public SizeParameters getSizeParameters() {
@@ -19,18 +21,36 @@ public class StandardContext implements Context {
     }
 
     @Override
+    public Object getApplicationData() {
+        return head(applicationDataStack).orElse(Unit.UNIT);
+    }
+
+    @Override
     public Context withSizeParameters(SizeParameters sizeParameters) {
-        return new StandardContext(cons(sizeParameters, sizeParametersStack));
+        return new StandardContext(cons(sizeParameters, sizeParametersStack), applicationDataStack);
+    }
+
+    @Override
+    public Context withApplicationData(Object applicationData) {
+        return new StandardContext(sizeParametersStack, cons(applicationData, applicationDataStack));
     }
 
     @Override
     public Context restoreSizeParameters() {
         return new StandardContext(uncons(sizeParametersStack)
-                .orElseThrow(() -> new IllegalStateException("SizeParameters stack is empty"))._2());
+                .orElseThrow(() -> new IllegalStateException("SizeParameters stack is empty"))._2(),
+                applicationDataStack);
     }
 
-    private static StandardContext context(Iterable<SizeParameters> sizeStack) {
-        return new StandardContext(sizeStack);
+    @Override
+    public Context restoreApplicationData() {
+        return new StandardContext(sizeParametersStack,
+                uncons(applicationDataStack)
+                        .orElseThrow(() -> new IllegalStateException("ApplicationData stack is empty"))._2());
+    }
+
+    private static StandardContext context(Iterable<SizeParameters> sizeStack, Iterable<Object> applicationDataStack) {
+        return new StandardContext(sizeStack, applicationDataStack);
     }
 
     public static StandardContext defaultContext() {
