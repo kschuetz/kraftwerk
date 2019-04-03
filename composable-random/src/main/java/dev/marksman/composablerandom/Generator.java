883969f6f21054d6f0cfacd381a248043dba0842
Kevin Schuetz <schuetzk@gmail.com>
2019-04-03 17:51:58 -0500

    Generator is now an interface

--- composable-random/src/main/java/dev/marksman/composablerandom/Generator.java
+++ composable-random/src/main/java/dev/marksman/composablerandom/Generator.java
@@ -10,35 +10,37 @@ import java.util.function.Function;
 import static dev.marksman.composablerandom.Instruction.flatMapped;
 import static dev.marksman.composablerandom.Instruction.mapped;
 
-@Value
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class Generator<A> implements Monad<A, Generator> {
-
-    private final Instruction<A> instruction;
+public interface Generator<A> extends Monad<A, Generator> {
+    Instruction<A> getInstruction();
 
     @Override
-    public final <B> Generator<B> fmap(Function<? super A, ? extends B> fn) {
-        return generator(mapped(fn, instruction));
+    default <B> Generator<B> fmap(Function<? super A, ? extends B> fn) {
+        return generator(mapped(fn, getInstruction()));
     }
 
     @Override
-    public final <B> Generator<B> flatMap(Function<? super A, ? extends Monad<B, Generator>> fn) {
+    default <B> Generator<B> flatMap(Function<? super A, ? extends Monad<B, Generator>> fn) {
         return generator(flatMapped(a -> ((Generator<B>) fn.apply(a))
                         .getInstruction(),
-                instruction));
+                getInstruction()));
     }
 
     @Override
-    public final <B> Generator<B> pure(B b) {
+    default <B> Generator<B> pure(B b) {
         return generator(Instruction.pure(b));
     }
 
-    public static <A> Generator<A> generator(Instruction<A> instruction) {
-        return new Generator<>(instruction);
+    static <A> Generator<A> generator(Instruction<A> instruction) {
+        return new DefaultGenerator<>(instruction);
     }
 
-    public static <A> Generator<A> constant(A a) {
+    static <A> Generator<A> constant(A a) {
         return generator(Instruction.pure(a));
     }
 
+    @Value
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    class DefaultGenerator<A> implements Generator<A> {
+        private final Instruction<A> instruction;
+    }
 }
