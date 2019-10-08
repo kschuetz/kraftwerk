cfe5ded7a505492cb198475561c88f5ca43557e7
Kevin Schuetz <schuetzk@gmail.com>
2019-10-07 21:00:13 -0500

    Remove Monad from GeneratorState

--- src/main/java/dev/marksman/composablerandom/GeneratorState.java
+++ src/main/java/dev/marksman/composablerandom/GeneratorState.java
@@ -1,33 +1,12 @@
 package dev.marksman.composablerandom;
 
 import com.jnape.palatable.lambda.functions.Fn1;
-import com.jnape.palatable.lambda.monad.Monad;
 
-import static dev.marksman.composablerandom.Result.result;
-
-public interface GeneratorState<A> extends Monad<A, GeneratorState<?>> {
+public interface GeneratorState<A> {
     Result<? extends Seed, A> run(Seed input);
 
     default Fn1<Seed, Result<? extends Seed, A>> getRun() {
         return this::run;
-    }
-
-    @Override
-    default <B> GeneratorState<B> fmap(Fn1<? super A, ? extends B> fn) {
-        return generator(getRun().fmap(a -> a.fmap(fn)));
-    }
-
-    @Override
-    default <B> GeneratorState<B> flatMap(Fn1<? super A, ? extends Monad<B, GeneratorState<?>>> fn) {
-        return generator(rs -> {
-            Result<? extends Seed, A> x = run(rs);
-            return ((GeneratorState<B>) fn.apply(x._2())).run(x._1());
-        });
-    }
-
-    @Override
-    default <B> GeneratorState<B> pure(B b) {
-        return generator(rs -> result(rs, b));
     }
 
     static <A> GeneratorState<A> generator(Fn1<Seed, Result<? extends Seed, A>> fn) {
