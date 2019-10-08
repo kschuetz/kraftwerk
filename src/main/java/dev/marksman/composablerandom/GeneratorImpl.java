bbc977d386c82de7f81a5220dc6f8c0fcc2b74bd
Kevin Schuetz <schuetzk@gmail.com>
2019-10-07 21:36:31 -0500

    Add runShrinking method

--- src/main/java/dev/marksman/composablerandom/GeneratorImpl.java
+++ src/main/java/dev/marksman/composablerandom/GeneratorImpl.java
@@ -1,5 +1,12 @@
 package dev.marksman.composablerandom;
 
+import dev.marksman.collectionviews.Vector;
+import dev.marksman.enhancediterables.ImmutableNonEmptyIterable;
+
 public interface GeneratorImpl<A> {
     Result<? extends Seed, A> run(Seed input);
+
+    default Result<? extends Seed, ImmutableNonEmptyIterable<A>> runShrinking(Seed input) {
+        return run(input).fmap(r -> Vector.of(r));
+    }
 }
