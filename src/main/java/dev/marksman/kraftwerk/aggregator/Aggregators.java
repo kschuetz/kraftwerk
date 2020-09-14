fa71f17d086e4db9376af1b712f001fda4d038ba
Kevin Schuetz <schuetzk@gmail.com>
2020-09-13 21:33:18 -0500

    Document Aggregator

--- src/main/java/dev/marksman/kraftwerk/aggregator/Aggregators.java
+++ src/main/java/dev/marksman/kraftwerk/aggregator/Aggregators.java
@@ -11,48 +11,95 @@ import java.util.Collection;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Id.id;
 import static dev.marksman.kraftwerk.aggregator.Aggregator.aggregator;
 
+/**
+ * A collection of built-in {@link Aggregator}s.
+ */
 public final class Aggregators {
 
     private Aggregators() {
 
     }
 
+    /**
+     * Creates an {@link Aggregator} for {@link ImmutableVector}s.
+     */
     public static <A> Aggregator<A, VectorBuilder<A>, ImmutableVector<A>> vectorAggregator() {
         return aggregator(VectorBuilder::builder, VectorBuilder::add, VectorBuilder::build);
     }
 
+    /**
+     * Creates an {@link Aggregator} for {@link ImmutableVector}s.
+     *
+     * @param initialCapacity a size hint for the final result.
+     *                        The size of the final result may end up being larger or smaller than this.
+     */
     public static <A> Aggregator<A, VectorBuilder<A>, ImmutableVector<A>> vectorAggregator(int initialCapacity) {
         return aggregator(() -> VectorBuilder.builder(initialCapacity), VectorBuilder::add, VectorBuilder::build);
     }
 
+    /**
+     * Creates an {@link Aggregator} for {@link String}s by concatenating other {@code String}s.
+     */
     public static Aggregator<String, StringBuilder, String> stringAggregator() {
         return aggregator(StringBuilder::new, StringBuilder::append, StringBuilder::toString);
     }
 
+    /**
+     * Creates an {@link Aggregator} for {@link String}s by concatenating other {@code String}s.
+     *
+     * @param initialCapacity a length hint for the final result.
+     *                        The size of the final result may end up being larger or smaller than this.
+     */
     public static Aggregator<String, StringBuilder, String> stringAggregator(int initialCapacity) {
         return aggregator(() -> new StringBuilder(initialCapacity), StringBuilder::append, StringBuilder::toString);
     }
 
+    /**
+     * Creates an {@link Aggregator} for {@link String}s by concatenating {@link Character}s.
+     */
     public static Aggregator<Character, StringBuilder, String> charAggregator() {
         return aggregator(StringBuilder::new, StringBuilder::append, StringBuilder::toString);
     }
 
+    /**
+     * Creates an {@link Aggregator} for {@link String}s by concatenating {@link Character}s.
+     *
+     * @param initialCapacity a length hint for the final result.
+     *                        The size of the final result may end up being larger or smaller than this.
+     */
     public static Aggregator<Character, StringBuilder, String> charAggregator(int initialCapacity) {
         return aggregator(() -> new StringBuilder(initialCapacity), StringBuilder::append, StringBuilder::toString);
     }
 
+    /**
+     * Creates an {@link Aggregator} for {@link String}s by concatenating {@link Maybe} values of {@code String}s.
+     */
     public static Aggregator<Maybe<String>, StringBuilder, String> maybeStringAggregator() {
         return aggregator(StringBuilder::new,
                 (builder, maybeString) -> builder.append(maybeString.orElse("")),
                 StringBuilder::toString);
     }
 
+    /**
+     * Creates an {@link Aggregator} for {@link String}s by concatenating {@link Maybe} values of {@code String}s.
+     *
+     * @param initialCapacity a length hint for the final result.
+     *                        The size of the final result may end up being larger or smaller than this.
+     */
     public static Aggregator<Maybe<String>, StringBuilder, String> maybeStringAggregator(int initialCapacity) {
         return aggregator(() -> new StringBuilder(initialCapacity),
                 (builder, maybeString) -> builder.append(maybeString.orElse("")),
                 StringBuilder::toString);
     }
 
+    /**
+     * Creates an {@link Aggregator} for any {@link Collection} type.
+     *
+     * @param constructCollection a function that creates an empty collection of type {@code C}
+     * @param <A>                 the element type
+     * @param <C>                 the collection type
+     * @return a {@code Collection} of type {@code C} with elements of type {@code A}
+     */
     public static <A, C extends Collection<A>> Aggregator<A, C, C> collectionAggregator(Fn0<C> constructCollection) {
         return aggregator(constructCollection, (collection, item) -> {
             collection.add(item);
@@ -60,6 +107,13 @@ public final class Aggregators {
         }, id());
     }
 
+    /**
+     * Creates an {@link Aggregator} out of a {@link Monoid}.
+     *
+     * @param monoid a {@code Monoid} for type {@code A}
+     * @param <A>    the element type and the type for the final result
+     * @return an {@code A}
+     */
     public static <A> Aggregator<A, A, A> monoidAggregator(Monoid<A> monoid) {
         return aggregator(monoid::identity, monoid::apply, id());
     }
